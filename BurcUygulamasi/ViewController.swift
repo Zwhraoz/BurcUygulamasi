eb41164f44be366cc20483e42001f2c375d821b8
Zwhraoz <Ozerzwhra@gmail.com>
2024-01-18 16:30:36 +0300

    Initial Commit

---
+++ BurcUygulamasi/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BurcUygulamasi
+//
+//  Created by zehra özer on 18.01.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
